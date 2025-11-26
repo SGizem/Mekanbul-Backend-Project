f58964f544769f828e9d19079f3744d720f9c965
SeGiOr38 <orulluoglu.gizem0438@gmail.com>
2025-11-26 22:57:20 +0300

    Sema dosyasindaki yazim hatalari duzeltildi ve guncellendi

--- app_api/models/venue.js
+++ app_api/models/venue.js
@@ -1,27 +1,23 @@
 var mongoose = require('mongoose');
-var hour=new mongoose.Schema(
-    {
-
-        day:{type:String,required:true},
-        open:{type:String,required:true},
-        close:{type:String,required:true},
-        isclosed:{type:Boolean,required:true}
-    }
-)
-var comment=new mongoose.Schema({
-    author:{type:String,required:true},
-    rating:{type:Number,min:0,max:5,required:true},
-    Date:{type:Date,default:Date.now},
-    text:{type:String,required:true}
+var hour = new mongoose.Schema({
+    days: { type: String, required: true },
+    open: { type: String, required: true },
+    close: { type: String, required: true },
+    isClosed: { type: Boolean, required: false }
+});
+var comment = new mongoose.Schema({
+    author: { type: String, required: true },
+    rating: { type: Number, min: 0, max: 5, required: true },
+    text: { type: String, required: true },
+    date: { type: Date, default: Date.now }
 });
 var venue = new mongoose.Schema({
     name: { type: String, required: true },
-    adress: String,
+    address: String,
     rating: { type: Number, min: 0, max: 5, default: 0 },
-    foodandrink:[String],
-    hours:[hour],
-    comments:[comment],
-    coordinates:{type:[Number],index:'2dsphere'},
+    foodanddrink: [String],
+    hours: [hour],
+    comments: [comment],
+    coordinates: { type: [Number], index: '2dsphere' }
 });
-
-mongoose.model('venue',venue, "venues");
+mongoose.model('venue', venue, 'venues');
