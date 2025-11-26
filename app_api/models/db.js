134585137f72bc4f10ac5a121356ab6f6a3dea7e
SeGiOr38 <orulluoglu.gizem0438@gmail.com>
2025-11-26 21:48:26 +0300

    localdeki veriler mongoDB Cloud a tasındı ve buraya mekan verileri eklendi

---
+++ app_api/models/db.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var dbURI = "mongodb+srv://SedefGizem:27sg@cluster0.rfsfstm.mongodb.net/mekanbul?appName=Cluster0"; 
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose ' + dbURI + ' adresindeki veritabanına bağlandı');
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose bağlantı hatası: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose bağlantısı kesildi');
+});
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log('Mongoose uygulama sonlandırma nedeniyle bağlantıyı kapattı');
+        process.exit(0);
+    });
+});
+
+require('./venue');
